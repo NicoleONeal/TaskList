a9f2ca999eed4d581d1f68846d3d4d0b179a3721
Nicole <nicoleoneal87@gmail.com>
2019-10-18 17:12:21 -0600

    start js setup

--- app/Models/List.js
+++ app/Models/List.js
@@ -1,4 +1,37 @@
 export default class List {
-    //TODO You will need to create a constructor 
-    //and the methods needed to create the view template for this model
+
+}
+
+constructor(data) {
+    this.taskName = data.taskName
+    this.tasks = data.tasks
+}
+
+getTemplate(index) {
+    let template = `
+    <div class="col-4 border"
+    <h1>${this.taskName}</h1>
+    <h4>${this.tasks}</h4>
+  `;
+    template += this.getTasks(index);
+    template += `
+    <form onsubmit="">
+     < div class = "input-group mb-3" >
+         <input
+            type = "text"
+             name = "taskName"
+            class = "form-control"
+            placeholder = "New Task"
+            aria - label = "New Task"
+             aria - describedby = "button-addon2"
+            required/>
+         <div class = "input-group-append">
+         <button class = "btn btn-outline-secondary" type = "submit" id = "button-addon2">
+         Add New Task </button> 
+         </div> 
+     </div> 
+    </form>
+    `
+
+
 }
